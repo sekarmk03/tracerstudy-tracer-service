3ee9516fabae8c254938949ea3375e31d666ebdd
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-05-23 15:58:28 +0700

    feat: add entity rekap userstudy by prodi

--- modules/userstudy/entity/userstudy_rekap_entity.go
+++ modules/userstudy/entity/userstudy_rekap_entity.go
@@ -25,3 +25,37 @@ func ConvertUserStudyRekapEntityToProto(usr *UserStudyRekap) *pb.UserStudyRekap 
 		UserStudyPercentage: usr.UserStudyPercentage,
 	}
 }
+
+type UserStudyRekapByProdi struct {
+	Nim                 string `json:"nim"`
+	Nama                string `json:"nama"`
+	TahunLulus          string `json:"tahun_lulus"`
+	PktsNamaAtasan      string `json:"pkts_nama_atasan"`
+	PktsEmailAtasan     string `json:"pkts_email_atasan"`
+	PktsInstansi        string `json:"pkts_instansi"`
+	UserstudyStatus     string `json:"userstudy_status"`
+	UserstudyNama       string `json:"userstudy_nama"`
+	UserstudyEmail      string `json:"userstudy_email"`
+	UserstudyInstansi   string `json:"userstudy_instansi"`
+	UserstudyJabatan    string `json:"userstudy_jabatan"`
+	LamaMengenalLulusan string `json:"lama_mengenal_lulusan"`
+	AverageGrade        string `json:"average_grade"`
+}
+
+func ConvertUserStudyRekapByProdiEntityToProto(usr *UserStudyRekapByProdi) *pb.UserStudyRekapByProdi {
+	return &pb.UserStudyRekapByProdi{
+		Nim:                 usr.Nim,
+		Nama:                usr.Nama,
+		TahunLulus:          usr.TahunLulus,
+		PktsNamaAtasan:      usr.PktsNamaAtasan,
+		PktsEmailAtasan:     usr.PktsEmailAtasan,
+		PktsInstansi:        usr.PktsInstansi,
+		UserstudyStatus:     usr.UserstudyStatus,
+		UserstudyNama:       usr.UserstudyNama,
+		UserstudyEmail:      usr.UserstudyEmail,
+		UserstudyInstansi:   usr.UserstudyInstansi,
+		UserstudyJabatan:    usr.UserstudyJabatan,
+		LamaMengenalLulusan: usr.LamaMengenalLulusan,
+		AverageGrade:        usr.AverageGrade,
+	}
+}
